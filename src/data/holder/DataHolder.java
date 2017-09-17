b4aee9e319afc6a774414471f422db54480be01e
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-09-17 20:29:11 +0200

    add dataHolder

--- src/data/holder/DataHolder.java
+++ src/data/holder/DataHolder.java
@@ -1,5 +1,29 @@
 package data.holder;
 
+import java.util.ArrayList;
+
+import data.model.Task;
+import data.model.User;
+
 public class DataHolder {
+	
+	private static ArrayList<User> user = new ArrayList<>();
+	private static ArrayList<Task> task = new ArrayList<>();
+
+	
+	public static ArrayList<User> getUser() {
+		return user;
+	}
+	public static void setUser(ArrayList<User> user) {
+		DataHolder.user = user;
+	}
+	public static ArrayList<Task> getTask() {
+		return task;
+	}
+	public static void setTask(ArrayList<Task> task) {
+		DataHolder.task = task;
+	}
+	
+	
 
 }
