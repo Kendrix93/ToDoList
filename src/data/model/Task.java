3af3c756aebebcc835485de032be1d8314b0f7af
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-09-17 20:27:04 +0200

    add task

--- src/data/model/Task.java
+++ src/data/model/Task.java
@@ -1,5 +1,153 @@
 package data.model;
 
 public class Task {
+	
+	private String projectName;
+	private String projectDescription;
+	private String taskName;
+	private String taskDescription;
+	private String taskStatus;
+	private String userLogin;
+	
+	
+	public Task (String projectName, String projectDescription, String taskName,
+			String taskDescription, String taskStatus, String userLogin) {
+		
+		this.projectName = projectName;
+		this.projectDescription = projectDescription;
+		this.taskName = taskName;
+		this.taskDescription = taskDescription;
+		this.taskStatus = taskStatus;
+		this.userLogin = userLogin;
+		
+	}
 
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+
+	public String getProjectDescription() {
+		return projectDescription;
+	}
+
+
+	public void setProjectDescription(String projectDescription) {
+		this.projectDescription = projectDescription;
+	}
+
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+
+
+	public String getTaskStatus() {
+		return taskStatus;
+	}
+
+
+	public void setTaskStatus(String taskStatus) {
+		this.taskStatus = taskStatus;
+	}
+
+
+	public String getUserLogin() {
+		return userLogin;
+	}
+
+
+	public void setUserLogin(String userLogin) {
+		this.userLogin = userLogin;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((projectDescription == null) ? 0 : projectDescription.hashCode());
+		result = prime * result + ((projectName == null) ? 0 : projectName.hashCode());
+		result = prime * result + ((taskDescription == null) ? 0 : taskDescription.hashCode());
+		result = prime * result + ((taskName == null) ? 0 : taskName.hashCode());
+		result = prime * result + ((taskStatus == null) ? 0 : taskStatus.hashCode());
+		result = prime * result + ((userLogin == null) ? 0 : userLogin.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (projectDescription == null) {
+			if (other.projectDescription != null)
+				return false;
+		} else if (!projectDescription.equals(other.projectDescription))
+			return false;
+		if (projectName == null) {
+			if (other.projectName != null)
+				return false;
+		} else if (!projectName.equals(other.projectName))
+			return false;
+		if (taskDescription == null) {
+			if (other.taskDescription != null)
+				return false;
+		} else if (!taskDescription.equals(other.taskDescription))
+			return false;
+		if (taskName == null) {
+			if (other.taskName != null)
+				return false;
+		} else if (!taskName.equals(other.taskName))
+			return false;
+		if (taskStatus == null) {
+			if (other.taskStatus != null)
+				return false;
+		} else if (!taskStatus.equals(other.taskStatus))
+			return false;
+		if (userLogin == null) {
+			if (other.userLogin != null)
+				return false;
+		} else if (!userLogin.equals(other.userLogin))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Task [projectName=" + projectName + ", projectDescription=" + projectDescription + ", taskName="
+				+ taskName + ", taskDescription=" + taskDescription + ", taskStatus=" + taskStatus + ", userLogin="
+				+ userLogin + "]";
+	}
+	
+	
+	
+		
 }
